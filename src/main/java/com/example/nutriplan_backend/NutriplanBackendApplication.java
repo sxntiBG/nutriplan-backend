7fbcab06d6b19330a4d9d1e764b67baaffeece89
sxntiBG <zapataospinasantiago8@gmail.com>
2025-11-06 01:19:26 -0500

    chore: configuración inicial del proyecto

---
+++ src/main/java/com/example/nutriplan_backend/NutriplanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutriplan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriplanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriplanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nutriplan_backend/NutriplanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutriplan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriplanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
