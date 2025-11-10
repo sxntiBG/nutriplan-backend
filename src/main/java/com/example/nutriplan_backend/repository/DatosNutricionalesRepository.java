00583ce8183178d88419085ca5e482d4ddf7269a
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-11-10 00:02:16 -0500

    feat: crear repository de DatosNutricionales

---
+++ src/main/java/com/example/nutriplan_backend/repository/DatosNutricionalesRepository.java
@@ -0,0 +1,11 @@
+package com.example.nutriplan_backend.repository;
+
+import com.example.nutriplan_backend.model.DatosNutricionales;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface DatosNutricionalesRepository extends JpaRepository <DatosNutricionales, Long> {
+    
+}
