3aed995f03ec614f9b099f2d9dec47ebd1f41570
joserodirguez18 <jmra0226@gmail.com>
2025-11-11 20:56:03 -0500

    Creación del Plan_Usuario, (Model, Repository, Service y Controller)

---
+++ src/main/java/com/example/nutriplan_backend/model/PlanUsuario.java
@@ -0,0 +1,42 @@
+package com.example.nutriplan_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "plan_usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_plan_usuario")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_objetivo")
+    private Objetivo objetivo;
+
+    @Column(name = "calorias_totales")
+    private Double caloriasTotales;
+
+    @Column(name = "carbohidratos_g")
+    private Double carbohidratosG;
+
+    @Column(name = "proteinas_g")
+    private Double proteinasG;
+
+    @Column(name = "grasas_g")
+    private Double grasasG;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+}
