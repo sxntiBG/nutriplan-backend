ee273f9afbe6ce128fd13aabc2fd1dd5abe85077
joserodirguez18 <jmra0226@gmail.com>
2025-11-13 19:48:59 -0500

    Optional del repository de Datos nutricionales

--- src/main/java/com/example/nutriplan_backend/repository/DatosNutricionalesRepository.java
+++ src/main/java/com/example/nutriplan_backend/repository/DatosNutricionalesRepository.java
@@ -1,11 +1,14 @@
 package com.example.nutriplan_backend.repository;
 
 import com.example.nutriplan_backend.model.DatosNutricionales;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface DatosNutricionalesRepository extends JpaRepository <DatosNutricionales, Integer> {
-    
+    Optional<DatosNutricionales> findByUsuarioId(Long usuarioId);
 }
